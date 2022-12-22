bf194f4cbc2b97458d92583eb93d71262654d3ba
Pritamdhar_official <pritamdhar6568@gmail.com>
2022-12-22 22:36:04 +0530

    Set  mongoose

--- server/index.js
+++ server/index.js
@@ -13,6 +13,7 @@ import { fileURLToPath}  from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
+const app = express();
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy:"cross-origin"}));
@@ -32,3 +33,13 @@ const storage = multer.diskStorage({
     }
   });
   const upload = multer({  storage });
+
+  const PORT=process.env.PORT || 6001;
+  mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser:true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>{
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
