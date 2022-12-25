489604e045d8af97ffee1fb9dc68c7f5561019fb
Pritamdhar_official <pritamdhar6568@gmail.com>
2022-12-25 10:53:23 +0530

    working on authentication system

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import { register } from "./controllers/auth.js";
 
 // configuration//
 const __filename = fileURLToPath(import.meta.url);
@@ -33,6 +34,8 @@ const storage = multer.diskStorage({
   }
 });
 const upload = multer({ storage });
+
+app.post("/auth/register",upload.single("picture"),register);
 
 const PORT = process.env.PORT || 6001;
 mongoose
