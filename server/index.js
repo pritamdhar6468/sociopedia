bf57f6fa0164ef6e3e5caf678b5da65be96965e3
Pritamdhar_official <pritamdhar6568@gmail.com>
2023-01-23 08:28:26 +0530

    update post section

--- server/index.js
+++ server/index.js
@@ -8,10 +8,13 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import  postRoutes from "./routes/post.js";
 import  authRoutes from "./routes/auth.js";
 import  usersRoutes from "./routes/users.js";
 
 import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
+import { verifyToken } from "./middleware/auth.js";
 
 // configuration//
 const __filename = fileURLToPath(import.meta.url);
@@ -39,11 +42,14 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 // routes with files
 app.post("/auth/register",upload.single("picture"),register);
+app.post("/posts",verifyToken,upload.single("picture"),createPost);
 
 
 // routes
 app.use("/auth", authRoutes);
 app.use("/users", usersRoutes);
+app.use("/posts", postRoutes);
+
 
 const PORT = process.env.PORT || 6001;
 mongoose
