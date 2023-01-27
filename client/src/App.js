9af4fd678fea1b52a7341cfc31525d09879df041
Pritamdhar_official <pritamdhar6568@gmail.com>
2023-01-27 13:42:00 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
-import './App.css';
+import { BrowserRouter, Navigate, Routes, Route} from 'react-router-dom';
+import homepage from 'scenes/homepage';
+import loginpage from 'scenes/loginpage';
+import profilepage from 'scenes/profilepage';
 
 function App() {
-  return (
-    <div className="app">
-     
-    </div>
-  );
+  return <div className="app"> 
+    
+      <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<loginpage/>} />
+            <Route path="/home" element={<homepage/>} />
+            <Route path="/profile/:userId" element={<profilepage/>} />
+        </Routes>
+      </BrowserRouter>
+   
+      </div>;
 }
 
 export default App;
