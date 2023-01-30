bcacb0466ac00667368dc4560674b9e7cd182781
Pritamdhar_official <pritamdhar6568@gmail.com>
2023-01-30 10:18:09 +0530

    import redux properties

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -38,7 +38,11 @@ export const authSlice = createSlice({
             const updatePosts = state.posts.map((post)=>{
                 if (post._id === action.payload.post_id) return action.payload.post;
                 return post;
-            })
-        }
-    }
-})
+            });
+            state.posts = updatePosts;
+        },
+    },
+});
+
+export const { setMode,setLogin,setLogout,setFriends,setPosts,setPost} = authSlice.actions;
+export default authSlice.reducer;
