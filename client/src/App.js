0dadd926c24438f74b0dc85638c12614c074f2dc
Pritamdhar_official <pritamdhar6568@gmail.com>
2023-01-31 11:47:29 +0530

    test client server working or not

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,31 @@ import { BrowserRouter, Navigate, Routes, Route} from 'react-router-dom';
 import homepage from 'scenes/homepage';
 import loginpage from 'scenes/loginpage';
 import profilepage from 'scenes/profilepage';
+import {useMemo} from 'react';
+import {useSelector} from 'react-redux';
+import {CssBaseline, ThemeProvider} from '@mui/material';
+import {createTheme} from '@mui/material/styles';
+import {themeSettings} from './theme';
+import state from 'state';
+
 
 function App() {
+
+
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   return <div className="app"> 
     
       <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
         <Routes>
             <Route path="/" element={<loginpage/>} />
             <Route path="/home" element={<homepage/>} />
             <Route path="/profile/:userId" element={<profilepage/>} />
         </Routes>
+        </ThemeProvider>
       </BrowserRouter>
    
       </div>;
--- client/src/theme.js
+++ client/src/theme.js
@@ -70,6 +70,34 @@ export const colorTokens = {
                     alt:colorTokens.grey[0]
                 }
             }),
-        }
-    }
- }
+        },
+        typography: {
+            fontFamily: ["Rubik", "sans-serif"].join(","),
+            fontSize: 12,
+            h1: {
+              fontFamily: ["Rubik", "sans-serif"].join(","),
+              fontSize: 40,
+            },
+            h2: {
+              fontFamily: ["Rubik", "sans-serif"].join(","),
+              fontSize: 32,
+            },
+            h3: {
+              fontFamily: ["Rubik", "sans-serif"].join(","),
+              fontSize: 24,
+            },
+            h4: {
+              fontFamily: ["Rubik", "sans-serif"].join(","),
+              fontSize: 20,
+            },
+            h5: {
+              fontFamily: ["Rubik", "sans-serif"].join(","),
+              fontSize: 16,
+            },
+            h6: {
+              fontFamily: ["Rubik", "sans-serif"].join(","),
+              fontSize: 14,
+            },
+        },
+    };
+ };
