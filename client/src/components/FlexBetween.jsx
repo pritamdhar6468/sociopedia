c4fde9e91a4004f0990f94c119325f6d6a24fcf1
Pritamdhar_official <pritamdhar6568@gmail.com>
2023-02-17 11:30:18 +0530

    create flexbetween file

---
+++ client/src/components/FlexBetween.jsx
@@ -0,0 +1,10 @@
+import { Box } from "@mui/material";
+import { styled } from "@mui/sysytem";
+
+const FlexBetween = styled( Box)({
+    display:"flex",
+    justifyContent:"space-between",
+    alignItems:"center"
+})
+
+export default FlexBetween;
