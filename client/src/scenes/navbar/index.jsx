70bde48a0d16ce01e3f1b18004844f673835c420
Pritamdhar_official <pritamdhar6568@gmail.com>
2023-03-14 20:07:12 +0530

    Update index.jsx

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -26,7 +26,21 @@ import { useNavigate } from "react-router-dom";
 import FlexBetween from "components/flexbetween";
 
 const Navbar =()=>{
-    return <div>Navbar</div>;
+    const [isMobileMenuToggled, setisMobileMenuToggled] = useState(false);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const user = userSelector((state)=> state.user);
+    const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+
+    const theme = useTheme();
+    const neutralLight = theme.palette.neutral.light;
+    const dark =theme.palette.neutral.dark;
+    const background =theme.palette.background.default;
+    const alt =theme.palette.background.alt;
+
+    const fullName = `${user.firstName} ${user.lastName}`
+
+
 };
 
 export default Navbar;
